6cd85903ccd7af27e0d5f9e7900aea2fa6b351ab
rennavittorio <renna.vittorio@gmail.com>
2023-02-07 14:02:10 +0100

    START file set up completed

---
+++ js/app.js
@@ -0,0 +1,14 @@
+// console.log('les go')
+
+/* DESCRIZIONE:
+Utilizziamo un ciclo per chiedere all’utente un numero tramite prompt.
+Se il numero è dispari, lo salviamo in una lista.
+Continuiamo a chiedere un numero all’utente finchè non avremo ottenuto 10 numeri dispari.
+Tutti gli altri valori li scartiamo (numeri pari ed eventuali NaN).
+Una volta ottenuti tutti i 10 numeri, stampiamoli in console uno alla volta. */
+
+/* BONUS:
+SHUFFLE ARRAY 
+a partire da una array con dei dati 
+creare un algoritmo che vada a mescolare gli elementi al suo interno
+*/
